a51c6f082a0938a750a0506a5c88ef670b2dbede
Divakar79953 <vamisettidivakar7@gmail.com>
2026-07-23 19:17:01 +0530

    Add Own Module

---
+++ Day5Tasks/Own Module/Main.py
@@ -0,0 +1,7 @@
+import Calculator
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print("Addition=",Calculator.add(a,b))
+print("Substraction=",Calculator.sub(a,b))
+print("Multipication=",Calculator.mul(a,b))
+print("Division=",Calculator.div(a,b))
