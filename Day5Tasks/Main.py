ffd76360dde3d2c069d71f79bf58409b18939fcd
Divakar79953 <vamisettidivakar7@gmail.com>
2026-07-25 11:32:39 +0530

    Add Day5Tasks

---
+++ Day5Tasks/Main.py
@@ -0,0 +1,10 @@
+from Packages import Math_operations
+from Packages import String_operations
+
+print(Math_operations.add(10,4))
+print(Math_operations.multiply(10,4))
+
+text="Divakar Naidu"
+
+print(String_operations.to_uppercase(text))
+print(String_operations.count_character(text))
