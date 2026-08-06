5e5def7bdf201a60322066f1463836b1946b026b
Divakar79953 <vamisettidivakar7@gmail.com>
2026-08-06 22:29:52 +0530

    Add Day10Tasks

---
+++ Day10Tasks/Arrays/Sorting_Product_Prices.py
@@ -0,0 +1,8 @@
+# Sorting Product Prices
+
+import numpy as np
+prices=[589,799,345,678,899]
+prices_array=np.array(prices)
+sorted_prices=np.sort(prices_array)
+print("Original Prices:",prices_array)
+print("Sorted Prices:",sorted_prices)
---
+++ Day10Tasks/Arrays/Temperature_Monitoring_System.py
@@ -0,0 +1,12 @@
+# Temperature Monitoring System
+import numpy as np
+
+temperature=np.array([[30,32,31],
+                      [29,33,34]])
+
+print("Temperature Data:")
+print(temperature)
+
+total=np.sum(temperature)
+
+print("Total Temperature:",total)
