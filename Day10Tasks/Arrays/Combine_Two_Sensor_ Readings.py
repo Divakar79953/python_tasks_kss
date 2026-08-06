7f8979687976b92eb1bba66b59d023135dc054ba
Divakar79953 <vamisettidivakar7@gmail.com>
2026-08-06 22:56:39 +0530

    Add Day10Tasks

--- Day10Tasks/Arrays/Combine_Two_Sensor_ Readings.py
+++
@@ -1,9 +0,0 @@
-# Combine Two Sensor Readings
-
-import numpy as np
-sensor1=np.array([12,34,56])
-sensor2=np.array([50,30,56)]
-combined=np.concatenate((sensor1,sensor2))
-print("Sensor1:",sensor1)
-print("Sensor2:",sensor2)
-print("Combined Array:",combined)
--- Day10Tasks/Arrays/Student_marks.py
+++
@@ -1,8 +0,0 @@
-# Student Marks Analysis
-
-import numpy as np
-marks=[94,93,56,72]
-marks_array=np.array(marks)
-updated_marks=marks_array+5
-print("Original Marks:",marks_array)
-print("Updated Marks:",updated_marks)
